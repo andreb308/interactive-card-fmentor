e06693d7b352732a337b8a689b904a2ad0fa8445
André Barros <107200960+andreb308@users.noreply.github.com>
2023-11-10 16:53:48 -0300

    fix: metadata + removing unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
 import DesktopBG from "./components/Background/DesktopBG";
 import FrontSection from "./components/FrontSection/Index";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  console.log("teste");
-
   return (
     <main>
       <FrontSection />
--- src/styles/FrontSection.ts
+++ src/styles/FrontSection.ts
@@ -4,8 +4,8 @@ export const Container = styled.div`
   display: flex;
   position: relative;
   /* grid-template-columns: 1fr 1fr; */
-  width: 90%;
-  height: 75dvh;
+  width: 80%;
+  height: 75%;
   /* background-color: rgba(100, 100, 100, 0.5); */
   z-index: 1;
 `;
